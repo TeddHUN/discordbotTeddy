8e74561771de5c7fdde4b77d70ed7fa09d92ba2f
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-18 13:25:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,16 @@ client.on('ready', () => {
     console.log('Elindult!');
     client.user.setStatus("dnd");
     client.user.setGame('Moderátori munka', "https://twitch.tv/teddhun");
-    i = 0;
-    setInterval(function(){ 
-        i++;
-        if(i == 1) {
-            client.user.setGame('Moderátori munka', "https://twitch.tv/teddhun");
-        } else if(i == 2) {
-            i = 0;
-            client.user.setGame('Rendszerfrissítés...');
-        }
-    
-    }, 60000); 
-   
+
+    setInterval(function(){    
+        client.user.setGame('Fejlesztés alatt!');
+        setInterval(function(){    
+            client.user.setGame('Moderátori munka', "https://twitch.tv/teddhun");    
+            setInterval(function(){    
+                client.user.setGame('Készülök..', "https://twitch.tv/teddhun");
+            }, 60000);
+        }, 60000);        
+    }, 60000);
 });
 
 client.on('message', message => {
