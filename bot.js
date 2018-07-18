952bd6b2615c1bde2fb6af482dd2c7a816bca63a
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-18 23:25:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,21 +10,14 @@ client.on('ready', () => {
     client.user.setGame('~tb help', "https://twitch.tv/teddhun");
 });
 
-
-const commands = {
-	'help': (msg) => {
-	 	msg.channel.send("Még fejleszt engem a fejlesztőm, légy türelemmel! ;)\n");	
-	}
-};
-
 client.on('message', msg => {		
 	if(message.author.bot) return;
-	if(message.content.indexOf(config.prefix) !== 0) return;
-	const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+	if(message.content.indexOf(prefix) !== 0) return;
+	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 	
 	if(command === "help") {
-		
+		message.channel.send("Még fejleszt engem a fejlesztőm, légy türelemmel! ;)");	
 	}
 });
 
