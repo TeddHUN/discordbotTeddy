1948d3ad3ed8356121f0437378072a68bacb7ba7
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-19 16:43:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,21 +9,21 @@ client.on('ready', () => {
     client.user.setGame('Fejlesztés alatt!', "https://twitch.tv/teddhun");
 });
 
-client.on('message', message => {		
+client.on('message', async message => {		
 	if(message.author.bot) return;
 	if(message.content.indexOf(prefix) !== 0) return;
 	const args = message.content.slice(prefix.length).trim().split(/ +/g);
 	const command = args.shift().toLowerCase();
 	
 	if(command === "help") {
-		message.channel.send("Még fejleszt engem a fejlesztőm, légy türelemmel! ;)");	
+		await message.channel.send("Még fejleszt engem a fejlesztőm, légy türelemmel! ;)");	
 	}	
 	
 	if(command === "makerangget") {
 		let guild = client.guilds.find("id", "464233102143651840");
 		let channel = guild.channels.find("id", "469283523283517440");
 		
-		channel.send("**Figyelem**, mostantól (2018.07.20) a játék és platform rang igénylések *automatikusan* zajlanak le!\n**Ahhoz**, hogy igényeld az egyik rangot írd be a `~tb rang` parancsot majd ezután 1 perced **van** reagálni, hogy melyiket is kéred!").then(sent => {
+		await channel.send("**Figyelem**, mostantól (2018.07.20) a játék és platform rang igénylések *automatikusan* zajlanak le!\n**Ahhoz**, hogy igényeld az egyik rangot írd be a `~tb rang` parancsot majd ezután 1 perced **van** reagálni, hogy melyiket is kéred!").then(sent => {
 			message.delete(1);	
 		});
 	}
@@ -33,8 +33,10 @@ client.on('message', message => {
 		let channel = guild.channels.find("id", "469283523283517440");
 		
 		if(message.channel === channel) {
-			let uzenet = channel.send(message.author + " Ahhoz, hogy megkapd az adott rangot válaszd ki a megfelelő Emojit!\n**Játék:**\n:one: The Crew\n:two: The Crew 2\n\n**Platform:**\n:three: PC\n:four: XBOX\n:five: PS").then(sent => {
+			let uzenet = await channel.send(message.author + " Ahhoz, hogy megkapd az adott rangot válaszd ki a megfelelő Emojit!\n**Játék:**\n:one: The Crew\n:two: The Crew 2\n\n**Platform:**\n:three: PC\n:four: XBOX\n:five: PS").then(sent => {
 				message.delete(1);
+				
+				await sent.react(":one:");
 				
 				sent.delete(10000);
 			});				
