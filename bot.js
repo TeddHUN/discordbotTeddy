e149ccbdae48d1a8a924253cabfa38159c098624
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-19 23:21:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -48,11 +48,11 @@ client.on('message', message => {
 			let guild = client.guilds.find("id", "352591575639130112");
 			let channel = guild.channels.find("id", "384300207933882370");		
 			
-			message.reply(message.author + ", Menetrend kiküldve!").then(sent => {
+			message.channel.send(message.author + ", Menetrend kiküldve!").then(sent => {
 				message.delete(1);
 				sent.delete(5000);
 				
-				var idoszak = "2018.07.16 - 2018.07.19";
+				var idoszak = "2018.07.20 - 2018.07.21";
 				var streamek = 2; //Streamek száma
 				//Visszafele kell megadni az adatokat
 				streamNev = [ 
@@ -97,33 +97,3 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
-/*const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-
-  message.channel.send({embed});*/
