db831678f14e1c913e036d2cf447f493cd59180a
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-23 16:34:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -160,16 +160,16 @@ client.on('message', message => {
 client.on('raw', event => {
     if (event.t === 'MESSAGE_REACTION_ADD' || event.t == "MESSAGE_REACTION_REMOVE"){
         
-        let channel = bot.channels.get(event.d.channel_id);
+        let channel = client.channels.get(event.d.channel_id);
         let message = channel.fetchMessage(event.d.message_id).then(msg=> {
         let user = msg.guild.members.get(event.d.user_id);
         
-        if (msg.author.id == bot.user.id && msg.content != initialMessage){
+        if (msg.author.id == client.user.id && msg.content != initialMessage){
        
             var re = `\\*\\*"(.+)?(?="\\*\\*)`;
             var role = msg.content.match(re)[1];
         
-            if (user.id != bot.user.id){
+            if (user.id != client.user.id){
                 var roleObj = msg.guild.roles.find('name', role);
                 var memberObj = msg.guild.members.get(user.id);
                 
