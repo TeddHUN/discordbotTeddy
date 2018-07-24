496ac46edf8d1a9c05d6b14c5e191588d4fa27d9
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2018-07-24 22:06:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 const ytdl = require("ytdl-core");
 
 var prefix = "-tb";
-var prefix2 = "#";
 
 function play(connection, message) {
 	var server = servers[message.guild.id];
@@ -39,142 +38,140 @@ client.on('ready', () => {
 
 client.on('message', message => {	
 	if(message.author.bot) return;
-	if(message.guild.id == 471294084732944406) {		
-		if(message.content.indexOf(prefix2) !== 0) return;
-		const args = message.content.slice(prefix2.length).trim().split(' ');
-		const command2 = args[0];
-		
-		if(command2 === "twitch") {
+	if(message.content.indexOf(prefix) !== 0) return;
+	const args = message.content.slice(prefix.length).trim().split(' ');
+	const command = args[0];
+	
+	if(command === "twitch") {
+		if(message.guild.id == 471294084732944406) {
 			message.channel.send(message.author + ", https://twitch.tv/teddhun");	
 		}
-		
-		if(command2 === "youtube") {		
+	}
+	
+	if(command === "youtube") {
+		if(message.guild.id == 471294084732944406) {		
 			message.channel.send(message.author + ", https://www.youtube.com/channel/UC2Lbgg1O-Qv9Bq-VV1g6SVw");	
 		}
-	} else {
-		if(message.content.indexOf(prefix) !== 0) return;
-		const args = message.content.slice(prefix.length).trim().split(' ');
-		const command = args[0];
+	}	
+	
+	if(command === "help") {
+		var commandtext = "# addstream\n* A parancsot csak a medvezér használhatja az élőadásainak kiíratására!";
+		message.channel.send(message.author + ", ```Mindenkinek:\n   -\n\nAdminisztrátoroknak:\n   " + prefix + " addstream	| Medvezér élőadásaihoz kapcsolódó menetrend kiírása.```\n\nÖrülök, hogy érdekel az amit tudok! ;)");		    
+	}	
 
-		if(command === "help") {
-			var commandtext = "# addstream\n* A parancsot csak a medvezér használhatja az élőadásainak kiíratására!";
-			message.channel.send(message.author + ", ```Mindenkinek:\n   -\n\nAdminisztrátoroknak:\n   " + prefix + " addstream	| Medvezér élőadásaihoz kapcsolódó menetrend kiírása.```\n\nÖrülök, hogy érdekel az amit tudok! ;)");		    
-		}	
-
-		if(command === "liga") {
-			message.channel.send(message.author + ", **Akutális ligák:**\n\nThe Crew 2 Liga #1 - http://www.thecrew2liga.teddhun.ml/ - https://discord.gg/YVnBaKv - https://goo.gl/forms/1scwYF9o7mWW1wUr1\nRocket League Liga #1 - *Hamarosan*\nPUBG Liga #1 - *Hamarosan*");
-		}
-
-		if(command === "makerangget") {
-			if(message.author.id == 312631597222592522) {	
-				let guild = client.guilds.find("id", "464233102143651840");
-				let channel = guild.channels.find("id", "470963699796934656");
-
-				message.delete(1);
-
-				var toSend = generateMessages();
-				let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
-				for (let mapObj of mappedArray){
-				    channel.send(mapObj[0]).then( sent => {
-					if (mapObj[1]){
-					  sent.react(mapObj[1]);  
-					} 
-				    });
-				}
-			}
-		}	
-
-		if(command === "play") {
-			if(!args[1]) {
-				message.channel.send(message.author + ", Elsőnek adj meg egy linket!");
-				return;
-			}
-
-			if(!message.member.voiceChannel) {
-				message.channel.send(message.author + ", Nem tudok oda menni hozzád!");
-				return;
-			}
-
-			//if(!servers[message.guild.id]) servers[message.guild.id] = {
-			//	queue: []
-			//};
-
-			//var server = servers[message.guild.id];
-
-			//server.queue.push(args[1]);
-			message.member.voiceChannel.join();
-			//if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
-				//play(connection, message);
-			//});
-		}
-
-		if(command === "skip") {
-			var server = servers[message.guild.id];
-
-			if(server.dispatcher) server.dispatcher.end();
-		}
-
-		if(command === "stop") {
-			var server = servers[message.guild.id];
-
-			if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();		
-		}
-
-		if(command === "addstream") {
-			if(message.author.id == 312631597222592522) {	
-				let guild = client.guilds.find("id", "352591575639130112");
-				let channel = guild.channels.find("id", "384300207933882370");		
-
-				message.channel.send(message.author + ", Menetrend kiküldve!").then(sent => {
-					message.delete(1);
-					sent.delete(5000);
-
-					var idoszak = "2018.07.23 - 2018.07.26";
-					var streamek = 3; //Streamek száma
-					//Visszafele kell megadni az adatokat
-					streamNev = [ 
-						"»PUBG« | Csirke szag, gránát robbanás... | #57 🐻",
-						"»PUBG« | Winner winner chickön dínner! | #56 🐻",
-						"»TC2« | Versengés New York-tól Los Angeles-ig! 🚗 | #11 🐻"
-					];
-
-					 streamDatum= [ 
-						"júl. 26., csütörtök 17:30 – 19:30",
-						"júl. 24., kedd 17:30 – 19:30",
-						"júl. 23., hétfő 17:30 – 19:30"
-					];
-
-					streamKep = [ 
-						"https://static-cdn.jtvnw.net/twitch-event-images-v2/2c3d16de-a808-431c-b109-e4359d283093-350x150",
-						"https://static-cdn.jtvnw.net/twitch-event-images-v2/3eb9c1ca-76cd-44c9-b518-bf40ebc38561-350x150",
-						"https://static-cdn.jtvnw.net/twitch-event-images-v2/1d24b925-c968-40dd-9c58-a39875e9ad5c-350x150"
-					];
-
-					streamJatek = [ 
-						"PLAYERUNKNOWN'S BATTLEGROUNDS",
-						"PLAYERUNKNOWN'S BATTLEGROUNDS",
-						"The Crew 2"
-					];
-
-					if(streamek > 1) {
-						channel.send("@everyone :new: Streamek a láthatáron!\n**Időszak:** " + idoszak);	
-					} else {
-						channel.send("@everyone :new: Új stream a láthatáron!\n**Időszak:** " + idoszak);
-					}				
-
-					for (i = 0; i < streamek ; i++) { 
-					    const embed = new Discord.RichEmbed()
-						.setColor(0x6441A4)
-						.setTitle(streamNev[i])
-						.setDescription("**Kezdés:** " + streamDatum[i] + "\n**Játék:** " + streamJatek[i] + "\n**Közvetítés helyszíne:** https://twitch.tv/teddhun" + "\n" + ":heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign:")
-						.setImage(streamKep[i]);
-
-					    channel.send({embed});
-					}
-				});
-			}
-		}
+	if(command === "liga") {
+		message.channel.send(message.author + ", **Akutális ligák:**\n\nThe Crew 2 Liga #1 - http://www.thecrew2liga.teddhun.ml/ - https://discord.gg/YVnBaKv - https://goo.gl/forms/1scwYF9o7mWW1wUr1\nRocket League Liga #1 - *Hamarosan*\nPUBG Liga #1 - *Hamarosan*");
 	}
+
+	if(command === "makerangget") {
+		if(message.author.id == 312631597222592522) {	
+			let guild = client.guilds.find("id", "464233102143651840");
+			let channel = guild.channels.find("id", "470963699796934656");
+
+			message.delete(1);
+
+			var toSend = generateMessages();
+			let mappedArray = [[toSend[0], false], ...toSend.slice(1).map( (message, idx) => [message, reactions[idx]])];
+			for (let mapObj of mappedArray){
+			    channel.send(mapObj[0]).then( sent => {
+				if (mapObj[1]){
+				  sent.react(mapObj[1]);  
+				} 
+			    });
+			}
+		}
+	}	
+
+	if(command === "play") {
+		if(!args[1]) {
+			message.channel.send(message.author + ", Elsőnek adj meg egy linket!");
+			return;
+		}
+
+		if(!message.member.voiceChannel) {
+			message.channel.send(message.author + ", Nem tudok oda menni hozzád!");
+			return;
+		}
+
+		//if(!servers[message.guild.id]) servers[message.guild.id] = {
+		//	queue: []
+		//};
+
+		//var server = servers[message.guild.id];
+
+		//server.queue.push(args[1]);
+		message.member.voiceChannel.join();
+		//if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection) {
+			//play(connection, message);
+		//});
+	}
+
+	if(command === "skip") {
+		var server = servers[message.guild.id];
+
+		if(server.dispatcher) server.dispatcher.end();
+	}
+
+	if(command === "stop") {
+		var server = servers[message.guild.id];
+
+		if(message.guild.voiceConnection) message.guild.voiceConnection.disconnect();		
+	}
+
+	if(command === "addstream") {
+		if(message.author.id == 312631597222592522) {	
+			let guild = client.guilds.find("id", "352591575639130112");
+			let channel = guild.channels.find("id", "384300207933882370");		
+
+			message.channel.send(message.author + ", Menetrend kiküldve!").then(sent => {
+				message.delete(1);
+				sent.delete(5000);
+
+				var idoszak = "2018.07.23 - 2018.07.26";
+				var streamek = 3; //Streamek száma
+				//Visszafele kell megadni az adatokat
+				streamNev = [ 
+					"»PUBG« | Csirke szag, gránát robbanás... | #57 🐻",
+					"»PUBG« | Winner winner chickön dínner! | #56 🐻",
+					"»TC2« | Versengés New York-tól Los Angeles-ig! 🚗 | #11 🐻"
+				];
+
+				 streamDatum= [ 
+					"júl. 26., csütörtök 17:30 – 19:30",
+					"júl. 24., kedd 17:30 – 19:30",
+					"júl. 23., hétfő 17:30 – 19:30"
+				];
+
+				streamKep = [ 
+					"https://static-cdn.jtvnw.net/twitch-event-images-v2/2c3d16de-a808-431c-b109-e4359d283093-350x150",
+					"https://static-cdn.jtvnw.net/twitch-event-images-v2/3eb9c1ca-76cd-44c9-b518-bf40ebc38561-350x150",
+					"https://static-cdn.jtvnw.net/twitch-event-images-v2/1d24b925-c968-40dd-9c58-a39875e9ad5c-350x150"
+				];
+
+				streamJatek = [ 
+					"PLAYERUNKNOWN'S BATTLEGROUNDS",
+					"PLAYERUNKNOWN'S BATTLEGROUNDS",
+					"The Crew 2"
+				];
+
+				if(streamek > 1) {
+					channel.send("@everyone :new: Streamek a láthatáron!\n**Időszak:** " + idoszak);	
+				} else {
+					channel.send("@everyone :new: Új stream a láthatáron!\n**Időszak:** " + idoszak);
+				}				
+
+				for (i = 0; i < streamek ; i++) { 
+				    const embed = new Discord.RichEmbed()
+					.setColor(0x6441A4)
+					.setTitle(streamNev[i])
+					.setDescription("**Kezdés:** " + streamDatum[i] + "\n**Játék:** " + streamJatek[i] + "\n**Közvetítés helyszíne:** https://twitch.tv/teddhun" + "\n" + ":heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign: :heavy_minus_sign:")
+					.setImage(streamKep[i]);
+
+				    channel.send({embed});
+				}
+			});
+		}
+	}	
 });
 
 client.on('raw', event => {
