114318dfdf37f31e6d7bb67b2e0b0c9b4cd5602c
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2019-04-06 01:42:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -700,8 +700,8 @@ TwitchMonitor.onChannelLiveUpdate((twitchChannel, twitchStream, twitchChannelIsL
     let guild = client.guilds.find("id", "547498318834565130");
     let targetChannel = guild.channels.find("id", "547557423318040603");//
     
-    var statusz = 0;
-    const uzenet = "";
+    let statusz = 0;
+    let uzenet = "";
 	
     con.query("SELECT * FROM streamerek WHERE twitch = '" + twitchChannel.display_name + "'", function (err, result) {
 	console.log(result[0].status + ", " + result[0].twitch);
