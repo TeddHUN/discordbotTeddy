8b68c560b6e9d69d4856774062ed1a30fc544138
TeddHUN <41347447+TeddHUN@users.noreply.github.com>
2019-04-07 11:52:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -298,8 +298,23 @@ ${serverQueue.songs.map(song => `**${++index} -** ${song.title} - Kérte: **${so
 				sent.delete(10000);					
 			});
 		
-		dUser.send("**Üzenet érkezett a `Sloth Gang` discord szerverről!**\n\nKüldő: " + msg.author + "\nÜzenet:" + dMessage);
 		msg.channel.send(msg.author + ", megsimizte " + dUser + "-t!").then(sent => {
+				msg.delete(1);					
+			});
+	}
+	
+	if(command === "jebemta") {		
+		
+		if(!msg.member.hasPermission("ADMINISTRATOR")) return msg.channel.send("**HIBA:** Ehhez nincs jogod!").then(sent => {
+				sent.delete(10000);					
+			});
+		
+		let dUser = msg.guild.member(msg.mentions.users.first()) || msg.guild.members.get(args[0]);
+		if (!dUser) return msg.channel.send("**Használat:** `--jebemta @Felhasználó").then(sent => {
+				sent.delete(10000);					
+			});
+		
+		msg.channel.send(msg.author + ", jebem ta " + dUser + "-t!").then(sent => {
 				msg.delete(1);					
 			});
 	}
